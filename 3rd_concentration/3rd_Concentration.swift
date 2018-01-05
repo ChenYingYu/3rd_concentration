2c2b4477071e9537b901b2758714151a7143337b
ChenYingYu <chenalan@ChenAlandeMacBook-Air.local>
2018-01-05 13:48:47 +0800

    use "Closure" and  "Extension" to simply oneAndOnlyFaceUpCard

--- 3rd_concentration/3rd_Concentration.swift
+++ 3rd_concentration/3rd_Concentration.swift
@@ -14,17 +14,18 @@ struct Concentration
         
     var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
+//            var foundIndex: Int?
+//            for index in cards.indices {
+//                if cards[index].isFaceUp {
+//                    if foundIndex == nil {
+//                        foundIndex = index
+//                    } else {
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
         }
         set {
             for index in cards.indices {
@@ -75,5 +76,11 @@ struct Concentration
         cards[0] = cards[randomIndex]
         cards[randomIndex] = shuffleCard
     }
+    }
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
